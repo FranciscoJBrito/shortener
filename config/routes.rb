722d05ba2f3449521315a9d0f85079d85ebe50cc
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-05-28 21:31:51 -0300

    changing root path for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticated :user do
+    root 'links#index', as: :authenticated_root
+  end
   root 'home#index'
   resources :links, only: %i[show destroy create index new]
   get '/:lookup_code' => 'links#show'
 end
-
