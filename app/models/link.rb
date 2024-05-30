8bdb47b9d032d29f50404e1c067e9655e20cc9ab
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-05-30 05:10:57 -0300

    adding relation between users and links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,10 @@
 class Link < ApplicationRecord
+  validates :original_url, presence: true
+  validates :lookup_code, presence: true
+  validates :name, presence: true
+  validate :original_url_format
+  belongs_to :user
+
   def original_url_format
     uri = URI.parse(original_url || '')
     return unless uri.host.nil?
@@ -7,6 +13,6 @@ class Link < ApplicationRecord
   end
 
   def shortened_url
-    "#{ENV.fetch('HOST', nil)}/#{lookup_code}"
+    "#{ENV.fetch('HOST', nil)}/s/#{lookup_code}"
   end
 end
