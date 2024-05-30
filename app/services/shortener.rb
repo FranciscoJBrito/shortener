d502e3cd382418c5142c34b2f3ff28b1c488bc95
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-05-30 05:16:43 -0300

    adding minimal changes in few areas

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -1,14 +1,15 @@
 class ShortenerService
-  attr_reader :url, :link_name, :link_model
+  attr_reader :url, :link_name, :user_id, :link_model
 
-  def initialize(url, link_name, link_model = Link)
+  def initialize(url, link_name, user_id, link_model = Link)
     @url = url
     @link_model = link_model
     @link_name = link_name
+    @user_id = user_id
   end
 
   def generate_short_link
-    link_model.create(original_url: url, lookup_code: lookup_code, name: link_name)
+    link_model.create(original_url: url, lookup_code: lookup_code, name: link_name, user_id: user_id)
   end
 
   def lookup_code
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'profile/index'
   devise_for :users
   authenticated :user do
     root 'links#index', as: :authenticated_root
   end
   root 'home#index'
   resources :links, only: %i[show destroy create index new]
-  get '/:lookup_code' => 'links#show'
+  get '/s/:lookup_code' => 'links#show'
 end
